45b04d1e3e9fad6af553dc226b1a8a6735670301
acchapm1 <acchapm1@asu.edu>
2023-04-06 12:04:40 -0700

    removed unneeded directories

--- monolith/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="monolith",
-    version="0.1",
-    packages=['monolith'],
-    scripts=['scripts/monolith'],
-    install_requires=['requests>=2.21'],
-)
